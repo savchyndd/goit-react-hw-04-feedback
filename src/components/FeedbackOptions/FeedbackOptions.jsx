d1debc4507de8f3d8a8dd505995484b2972fa994
Dmytro <savchyndd@gmail.com>
2023-05-10 21:04:57 +0200

    del clg

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { OptionsList, OptionsItem, Button } from './FeedbackOptions.module';
 
 export const FeedbackOptions = ({ options, leaveFeedback }) => {
-  console.log(options);
   return (
     <OptionsList>
       {options.map(option => (
